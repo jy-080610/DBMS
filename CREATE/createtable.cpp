5635c1c70ee6c35b873483812321fe583718cb00
kyx080610 <1443088109@qq.com>
2023-04-05 00:02:46 +0800

    2023/4/5 by liuixiao - createtable

---
+++ CREATE/createtable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus on 2023/4/4.
+//
+
+#include "createtable.h"
---
+++ CREATE/createtable.h
@@ -0,0 +1,14 @@
+//
+// Created by Asus on 2023/4/4.
+//
+
+#ifndef DBMS_CREATETABLE_H
+#define DBMS_CREATETABLE_H
+
+
+class createtable {
+
+};
+
+
+#endif //DBMS_CREATETABLE_H
