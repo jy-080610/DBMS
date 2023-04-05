80a31c0329fbfa757225a8e38d8001ac807a776b
kyx080610 <1443088109@qq.com>
2023-04-05 14:52:23 +0800

    2023/4/5 by liuixiao 使用正则表达式匹配输入语句

---
+++ analysissql.h
@@ -0,0 +1,18 @@
+//
+// Created by Asus on 2023/4/5.
+//
+
+#ifndef DBMS_ANALYSISSQL_H
+#define DBMS_ANALYSISSQL_H
+#include "qstringlist.h"
+
+class AnalysisSQL{
+public:
+    AnalysisSQL();
+    QStringList resolveSql(QString sql);
+private:
+    QStringList sqlList;//保存所有可能用到的正则表达式
+};
+
+
+#endif //DBMS_ANALYSISSQL_H
