7fb278011fd4b60736d3fcd5e1e94d66d4e82f15
geniusaaaaaa <2504386283@qq.com>
2023-04-17 23:32:27 +0800

    测试部分功能

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <QApplication>
 #include <QPushButton>
 #include "mainwindow.h"
+#include "table.h"
+#include "landing.h"
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 //    QPushButton button("Hello world!", nullptr);
 //    button.resize(200, 100);
 //    button.show();
+
+//    Mainwindow w;
+//    w.show();
+//    table t;
+//    t.show();
+    landing l;
+    l.show();
     Mainwindow w;
     //这边把主窗口的show()函数注释，这样主窗口才不会自动打开，
     //必须通过登录窗口发出的信号，才能启动主窗口的show()函数。
