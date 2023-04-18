069a1fe4a16f302810c4e367c26feb4d8512f5f0
geniusaaaaaa <2504386283@qq.com>
2023-04-18 19:20:29 +0800

    landing修改

--- landing.h
+++ landing.h
@@ -6,7 +6,11 @@
 #define DBMS_LANDING_H
 
 #include <QWidget>
-extern QString name; // 创建全局变量用户名
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QByteArray>
+#include<QCheckBox>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class landing; }
@@ -20,9 +24,21 @@ public:
 
     ~landing() override;
 
+    void read_json();//读json
+    void write_json();//写json
+    void message_init(QString flag1,QString flag2);//根据json内容决定是否填入输入框
+
+
+signals:
+    //登录mainwindow主界面信号
+    void login();
+    //关闭登录界面信号
+    void close_window();
+public slots:
+    //登录按钮后触发的事件
+    void logButton_clicked();
 private:
     Ui::landing *ui;
 };
-
 
 #endif //DBMS_LANDING_H
--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[]) {
 //    QPushButton button("Hello world!", nullptr);
 //    button.resize(200, 100);
 //    button.show();
+    landing l;
+    l.show();
     Mainwindow w;
     //这边把主窗口的show()函数注释，这样主窗口才不会自动打开，
     //必须通过登录窗口发出的信号，才能启动主窗口的show()函数。
