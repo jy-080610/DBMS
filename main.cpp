b0278f0f251dee335e0925bb1f32b409c1245274
2003Fu <21301305@bjtu.edu.cn>
2023-04-19 21:08:18 +0800

    SQL语句管理表

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "mainwindow.h"
 #include"dbmanager.h"
 #include"tablemanager.h"
+#include "managetablebysql.h"
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 //    QPushButton button("Hello world!", nullptr);
@@ -14,9 +15,12 @@ int main(int argc, char *argv[]) {
     //dbmanager db;
     //db.myCreateDataBase("test");
     //db.myDeteleDataBase("test");
-    TableManager tb("wende","test");
-    tb.tableCreator("table1");
-
+    //TableManager *tb=new TableManager("wende","test");
+    //tb->tableDelete("table1");
+    //TableManager tb("wende","test");
+    //tb.tableDelete("table1");
+    ManageTableBySql t;
+    t.deleteTable("test1");
     //这边把主窗口的show()函数注释，这样主窗口才不会自动打开，
     //必须通过登录窗口发出的信号，才能启动主窗口的show()函数。
     //w.show();
