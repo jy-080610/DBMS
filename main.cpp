77de3a3fda3de25303e0253c3f6c8652183c1b66
geniusaaaaaa <2504386283@qq.com>
2023-04-19 22:08:32 +0800

    表的创建与删除1

--- main.cpp
+++ main.cpp
@@ -21,8 +21,8 @@ int main(int argc, char *argv[]) {
     //tb->tableDelete("table1");
     //TableManager tb("wende","test");
     //tb.tableDelete("table1");
-    ManageTableBySql t;
-    t.deleteTable("test1");
+    FieldManager f;
+    f.show();
     //这边把主窗口的show()函数注释，这样主窗口才不会自动打开，
     //必须通过登录窗口发出的信号，才能启动主窗口的show()函数。
     //w.show();
