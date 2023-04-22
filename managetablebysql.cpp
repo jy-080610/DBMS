5a5203dccaa81a9d862c9421d3f4b8000318bc91
kyx080610 <1443088109@qq.com>
2023-04-22 20:48:19 +0800

    表的创建与删除

--- managetablebysql.cpp
+++ managetablebysql.cpp
@@ -1,5 +1,73 @@
 //
-// Created by Asus on 2023/4/17.
+//表的创建与删除
 //
 
 #include "managetablebysql.h"
+#include "qdir.h"
+#include "qdebug.h"
+#include "tablemanager.h"
+#include "qmessagebox.h"
+#include "privilegemanager.h"
+managetablebysql::managetablebysql() {
+
+}
+//创建表
+void managetablebysql::createTable(QString tableName) {
+    QDir *dir = new QDir(QDir::currentPath());
+    dir->cdUp();
+    QString dirPath = dir->path() + "/DBMS/data/sys/curuse.txt";
+    QFile file(dirPath);
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "文件打开失败";
+    }
+    QTextStream read(&file);
+    QStringList list;
+    QString str = read.readLine();
+    list = str.split(",");
+    file.close();
+    if (tableName != "") {
+        TableManager TM(list[0], list[1]);
+        TM.tableCreator(tableName);
+        QMessageBox::information(0,
+                                 "通知",
+                                 "表已经创建",
+                                 QMessageBox::Ok | QMessageBox::Default,
+                                 QMessageBox::Cancel | QMessageBox::Escape, 0);
+    } else {
+        QMessageBox::critical(0, "错误", "表名不能为空",
+                              QMessageBox::Ok | QMessageBox::Default,
+                              QMessageBox::Cancel | QMessageBox::Escape, 0);
+    }
+
+}
+//删除表
+void managetablebysql::deleteTable(QString tableName) {
+    QDir *dir = new QDir(QDir::currentPath());
+    dir->cdUp();
+    QString dirPath = dir->path() + "/DBMS/data/sys/curuse.txt";
+
+    QFile file(dirPath);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "文件打开失败";
+    }
+    QTextStream read(&file);
+    QStringList list;
+    QString     str = read.readLine();
+    list = str.split(",");
+    file.close();
+
+    if (tableName != "") {
+        TableManager TM(list[0], list[1]);
+        TM.tableDelete(tableName);
+        QMessageBox::information(0,
+                                 "通知",
+                                 "表已经删除",
+                                 QMessageBox::Ok | QMessageBox::Default,
+                                 QMessageBox::Cancel | QMessageBox::Escape, 0);
+    } else {
+        QMessageBox::critical(0, "错误", "表名不能为空",
+                              QMessageBox::Ok | QMessageBox::Default,
+                              QMessageBox::Cancel | QMessageBox::Escape, 0);
+    }
+}
--- managetablebysql.h
+++ managetablebysql.h
@@ -1,13 +1,16 @@
 //
-// Created by Asus on 2023/4/17.
+//
 //
 
 #ifndef DBMS_MANAGETABLEBYSQL_H
 #define DBMS_MANAGETABLEBYSQL_H
-
+#include "QString"
 
 class managetablebysql {
-
+public:
+    managetablebysql();
+   void createTable(QString);
+    void deleteTable(QString);
 };
 
 
